70af55f5594d8ca6f31d42db51bdaa94bf7fce34
Nazarchyk Bogdan <bodjech2676@gmail.com>
2025-09-19 15:56:26 +0300

    task2: divide and subtract

--- main.py
+++ main.py
@@ -15,6 +15,16 @@ def multiply(a: float, b: float) -> float:
     return a * b
 
 
+def subtract(a: float, b: float) -> float:
+    """Multiplies two numbers."""
+    return a - b
+
+
+def divide(a: float, b: float) -> float:
+    """Multiplies two numbers."""
+    return a / b
+
+
 # Завантажуємо змінні оточення з файлу .env
 load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
@@ -23,23 +33,23 @@ api_key = os.getenv("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 print("Модель успішно ініціалізовано!")
 
-# Надсилаємо промпт до моделі
-response = client.models.generate_content(
-    model="gemini-2.0-flash",
-    contents="Поясни концепцію рекурсії в програмуванні, використовуючи аналогію з ляльками-матрьошками.",
-)
-# Виводимо текстову відповідь
-print(response.text)
+# # Надсилаємо промпт до моделі
+# response = client.models.generate_content(
+#     model="gemini-2.0-flash",
+#     contents="Поясни концепцію рекурсії в програмуванні, використовуючи аналогію з ляльками-матрьошками.",
+# )
+# # Виводимо текстову відповідь
+# print(response.text)
 
 # Надсилаємо запит, який має викликати функцію
-prompt = "Скільки буде, якщо 5 помножити на 12, а потім додати 8?"
+# prompt = "Скільки буде, якщо 5 помножити на 12, а потім додати 8?"
+prompt = "Скільки буде 100 поділити на 4?"
 response = client.models.generate_content(
     model="gemini-2.0-flash",
-    contents="Скільки буде, якщо 5 помножити на 12, а потім додати 8?",
+    contents=prompt,
     config=types.GenerateContentConfig(
-        tools=[add, multiply],
+        tools=[add, multiply, subtract, divide],
     ),
 )
 
-# function_call = response.candidates[0].content.parts[0].function_call
 print(f"Фінальна текстова відповідь: {response.text}")
